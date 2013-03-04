d055d5cb5dd10d5b1d5fff8ccb7a1aea475e956a
Anubhaw Arya <anubhaw.arya@gmail.com>
2013-03-04 16:16:39 -0500

    First commit

---
+++ src/com/example/MIIOW/MyActivity.java
@@ -0,0 +1,12 @@
+package com.example.MIIOW;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
